ceb733f5b8aec0fa823027e390f618c1d663d3b5
Aquiles Trindade <trindadedev13@gmail.com>
2025-02-13 01:13:58 -0300

    feat: Throwable.getStackTrace ext

---
+++ src/main/java/org/amix/ext/ThrowableExt.kt
@@ -0,0 +1,39 @@
+package org.amix.ext
+
+/*
+ *  This file is part of Amix © 2024.
+ *
+ *  Amix is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Amix is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with Amix.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import android.os.Process
+import android.util.Log
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.io.Writer
+
+/**
+ * Function to get the stack trace.
+ */
+fun Throwable?.getStackTrace(): String {
+  val result: Writer = StringWriter()
+  PrintWriter(result).use { printWriter ->
+    var throwable: Throwable? = this
+    while (throwable != null) {
+      throwable.printStackTrace(printWriter)
+      throwable = throwable.cause
+    }
+  }
+  return result.toString()
+}
